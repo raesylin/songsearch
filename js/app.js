0d440023143706353d71c6acd4eae1acc85ec297
Rachel Lin <raesylin@gmail.com>
2016-09-15 12:12:34 -0700

    add slider support

---
+++ js/app.js
@@ -0,0 +1,91 @@
+'use strict';
+
+$.noConflict();
+
+var app = angular.module('app', ['rzModule'])
+.controller('mainCtrl', ['$scope', '$http', function($scope, $http){
+	$scope.song_name = '';
+	$scope.artist_name = '';
+	$scope.showWormhole = false;
+	$scope.showGuitar = false;
+	$scope.showForm2 = false;
+	$scope.showResults = false;
+	$scope.numRecSongs = null;
+	$scope.numSameArtist = null;
+	$scope.songMatches = [];
+	$scope.songSelection = null;
+	$scope.reccommendedSongs = [];
+	$scope.userName = '';
+	$scope.simUserParam = 0.5;
+
+	$scope.slider = {
+		value: 10,
+		options: {
+			floor: 0,
+			ceil: 10,
+			step: 1,
+			onChange: function(id, model, type) {
+				$scope.slider.modelValue = model;  // this is the value selected by the slider;you can upload this to the server.
+			}
+		}
+	};
+
+	$scope.query = function() {
+		
+		// clear data
+		$scope.songMatches = [];
+		$scope.reccommendedSongs = [];
+		// hide form 2 view
+		$scope.showForm2 = false; 
+		$scope.showResults = false;
+
+		$scope.showWormhole = true;
+		// query url for form 1
+		var url1 = 'http://10.11.255.204:9010/SongSelection?song_name=' + $scope.song_name + '&artist_name=' + $scope.artist_name;
+
+		// ajax request
+		$http.get(url1)
+			.then(function(result) {
+				$scope.songMatches = result.data;
+//				console.log($scope.songMatches);
+				$scope.showWormhole = false;
+				$scope.showForm2 = true;
+			}, function(error){
+				console.log("Failed to load song matches: " + error);
+			});
+	};
+	
+	$scope.query2 = function() {
+
+		$scope.showGuitar=true;
+		$scope.showResult=false;
+		$scope.reccommendedSongs = [];
+		$scope.numRecSongs = $scope.numRecSongs || "10";
+		$scope.numSameArtist = $scope.numSameArtist || "3";
+
+		var refSongId = $scope.songMatches[$scope.songSelection].songId;
+		var refArtistId = $scope.songMatches[$scope.songSelection].artistId;
+//		console.log($scope.songSelection);
+//		console.log('username = ' + $scope.userName);
+//		console.log('refArtistId = ' + refArtistId);
+//		console.log('numRecSongs = ' + $scope.numRecSongs);
+//		console.log('numSameArtist = ' + $scope.numSameArtist);
+		
+		var url2 = 'http://10.11.255.204:9010/SongRecommendation?song_key='+refSongId+'&artist_key='
+			+refArtistId +'&num_recs=' + $scope.numRecSongs + '&num_same_artist='+$scope.numSameArtist 
+			+ '&user_name='+$scope.userName + '&sim_user_param='+$scope.simUserParam;  // the query url for form 2
+
+		// ajax request
+		$http.get(url2)
+			.then(function(result) {
+//				console.log(result)
+				$scope.recommendedSongs = result.data;
+//	console.log($scope.recommendedSongs);
+				$scope.showGuitar=false;
+				$scope.showResults = true; // show query result view
+			}, function(error) {
+				console.log('Failed to load recommended Songs: ' + error);
+			});
+};
+
+}]);
